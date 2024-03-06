eb4187a1cee1766561be93eac0d6602915d2db32
moreshwarsahu <moreshwars720@gmail.com>
2024-03-07 00:31:03 +0530

    user push

--- index.js
+++ index.js
@@ -7,20 +7,51 @@ require('dotenv').config();
 
 require('./db_connections/conn.js')
 
+
 //schema;
-const users = require("./schema/personal_detail.js")
+const personal_detail = require("./schema/personal_detail.js")
 
 //middlwares
 app.use(express.json());
+
+
+
+//routing
 
 app.get('/',(req,res)=>{
     res.send ("port is running")
 })
 
 app.get('/users',(req,res)=>{
-
+res.send('get your user data here')
 
 })
+
+//post
+// app.post('/api/personal-details', async (req, res) => {
+//     try {
+//       const { name, phone_no, email } = req.body;
+  
+//       // Create a new instance of the PersonalDetail model
+//       const newPersonalDetail = new PersonalDetail({
+//         name,
+//         phone_no,
+//         email,
+//       });
+  
+//       // Save the new personal detail to the database
+//       await newPersonalDetail.save();
+  
+//       res.status(201).json({ message: 'Personal details saved successfully' });
+//     } catch (error) {
+//       console.error('Error saving personal details:', error);
+//       res.status(500).json({ message: 'Internal Server Error' });
+//     }
+//   });
+
+
+
+
 
 app.listen(PORT, ()=>{
     console.log('port is running')
