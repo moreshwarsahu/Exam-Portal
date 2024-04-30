98b5828d49ed05d745e5cf4dccb3b0f574c84853
moreshwarsahu <moreshwars720@gmail.com>
2024-04-30 08:49:59 +0530

    updated

--- index.js
+++ index.js
@@ -339,30 +339,21 @@ app.get('/fetch_questions', async (req, res) => {
     
      const { school_id } = req.query;
  
-     const questionPapers = await question_paper.find({ school_id});
-     const examIds = questionPapers.map(paper => paper._id);
+     const questionPapers = await question_paper.find({ school_id });
      const questionIds = questionPapers.map(paper => paper.question_id);
-    
+ 
      const allQuestionIds = [].concat(...questionIds);
 
      const questions = await question_bank.find({
        _id: { $in: allQuestionIds }
      });
  
-     res.status(200).json({
-       status: 'success',
-       status_code: 200,
-       message: 'Questions fetched successfully',
-       data: questions,
-       school_id,
-       examIds
-     });
+     res.status(200).json({ status: 'success', status_code: 200, message: 'Questions fetched successfully', data: questions, school_id });
   } catch (error) {
      console.error('Error fetching questions by SPOC ID:', error);
      res.status(500).json({ status: 'failure', status_code: 500, message: 'Internal Server Error' });
   }
  });
-
 
  app.get('/exam_details', async (req, res) => {
   try {
